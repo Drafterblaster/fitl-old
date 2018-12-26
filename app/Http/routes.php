90b45be5a3975db5460d84079a5ba7d0ec61b559
Darryl Adair <accesscarousel@gmail.com>
2018-12-26 09:09:59 -0800

    initial laravel application with a static page controller and about page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,3 +20,5 @@ Route::get('welcome', function () {
 });
 
 Route::get('about', 'PageController@about');
+
+Route::get('contact', 'PageController@contact');
---
+++ resources/views/pages/contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Contact</title>
+	</head>
+	<BODY>
+		<H1>Contact me</H1>
+		<p>Here is how the world can know I have existed</p>
+	</BODY>
+</html>
